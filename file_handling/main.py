e11743248c17cd0f590897466fd5761c64d7f13c
copasnehakumari <reetasneha006@gmail.com>
2026-04-30 22:57:10 -0700

    Add files via upload

---
+++ file_handling/main.py
@@ -0,0 +1,50 @@
+
+
+
+"""file=open("text.txt","r")
+print(file.read())
+file.close()
+
+
+file=open("text1.txt","w")
+file.write( "hello")
+file.close()
+
+file=open("text1.txt","a")
+file.write( "hello word")
+file.close()
+
+file=open("text2.txt","x")
+file.write( "hello ")
+file.close()
+
+
+file=open("python.py","x")
+file.write( "for i in range(1,11):print (i)")
+
+file.close()"""
+
+
+
+
+"""for i in range(1,11):
+ file=open(f"python{i}.py", "x")
+ file.write( f"file number{i}")
+
+print("file create")"""
+
+"""for i in range(1,11):
+ file=open("tex"+str(i)+".txt","x")
+ file.write( "file number"+str(i))
+
+print("file create")"""
+
+"""import os
+print(os.remove("python1.py"))
+import os
+for i in range(1,11):
+ filename=f"tex{i}.txt"
+ print(os.remove(filename))"""
+    
+         
+  
